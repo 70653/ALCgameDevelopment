9971ec220a3256e5702b21334bbca9889e11a610
Unknown <porter.g829@stu.nebo.edu>
2018-09-05 10:28:36 -0600

    created level 0 with script

---
+++ 2D Game/Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour {
+
+public int playerX, playerY, moveSpeed = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
